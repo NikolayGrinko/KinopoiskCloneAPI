8581d287babe5e2059827cd5943ae5454d19bbfe
grinya37 <112849355+grinya37@users.noreply.github.com>
2023-10-15 00:42:30 +0300

    setting_Container_NameUser_add_Buttons

--- KinopoiskCloneAPI/ViewController/General/ProfileUIView.swift
+++ KinopoiskCloneAPI/ViewController/General/ProfileUIView.swift
@@ -9,7 +9,6 @@ import Foundation
 import UIKit
 
 
-
 class ProfileUIView: UIView {
     
     let containerView: UIView = {
@@ -28,16 +27,7 @@ class ProfileUIView: UIView {
         return container
     }()
     
-    //    var gradientLayer: CAGradientLayer! {
-    //        didSet {
-    //            gradientLayer.startPoint = CGPoint(x: 0, y: 0)
-    //            gradientLayer.endPoint = CGPoint(x: 0, y: 1)
-    //            let startColor = #colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1).cgColor
-    //            let endColor = #colorLiteral(red: 0.1411764771, green: 0.3960784376, blue: 0.5647059083, alpha: 1).cgColor
-    //            gradientLayer.colors = [startColor, endColor]
-    //            //   gradientLayer.locations = [0, 0.2, 1]
-    //        }
-    //    }
+  
     
     private let myImageAvat: UIImageView = {
         let imageView = UIImageView()
@@ -70,33 +60,53 @@ class ProfileUIView: UIView {
         return nameAvatar
     }()
     
-    private let imageBabyAvatar: UIImageView = {
-        let imageView = UIImageView()
-        imageView.image = UIImage(named: "baby")
-        imageView.contentMode = .center
-        imageView.frame = CGRect(x: 20, y: 220, width: 40, height: 40)
-        imageView.layer.cornerRadius = 20
-        imageView.layer.masksToBounds = true
-        return imageView
+    private let babyButton: UIButton = {
+        var configuration = UIButton.Configuration.filled()
+        configuration.image = UIImage(named: "baby")
+        // между контентом
+        configuration.imagePadding = 10
+        
+        configuration.imagePlacement = .leading
+        configuration.titleAlignment = .trailing
+        configuration.baseBackgroundColor = .systemGray6
+        configuration.attributedTitle = AttributedString("Дети  12+", attributes: AttributeContainer([NSAttributedString.Key.foregroundColor: UIColor(white: 230/120, alpha: 1)]))
+        configuration.buttonSize = .large
+        configuration.contentInsets = .init(top: 5, leading: 10, bottom: 5, trailing: 100)
+        configuration.cornerStyle = .large
+        let button = UIButton(configuration: configuration, primaryAction: UIAction(handler: { action in
+          //debugPrint("Tapped One Button!")
+        }))
+        button.frame = CGRect(x: 20, y: 220, width: 220, height: 50)
+        return button
     }()
     
-    private let nameBabyLabel: UILabel = {
-        let nameAvatar = UILabel()
-        nameAvatar.text = "Дети"
-        nameAvatar.frame = CGRect(x: 80, y: 220, width: 50, height: 20)
-        nameAvatar.font = .boldSystemFont(ofSize: 18)
-        nameAvatar.textColor = .white
-        return nameAvatar
-    }()
-    
-    private let nameAgeLabel: UILabel = {
-        let nameAvatar = UILabel()
-        nameAvatar.text = "12+"
-        nameAvatar.frame = CGRect(x: 80, y: 240, width: 50, height: 20)
-        nameAvatar.font = .boldSystemFont(ofSize: 14)
-        nameAvatar.textColor = .systemGray
-        return nameAvatar
-    }()
+//    private let imageBabyAvatar: UIImageView = {
+//        let imageView = UIImageView()
+//        imageView.image = UIImage(named: "baby")
+//        imageView.contentMode = .center
+//        imageView.frame = CGRect(x: 20, y: 220, width: 40, height: 40)
+//        imageView.layer.cornerRadius = 20
+//        imageView.layer.masksToBounds = true
+//        return imageView
+//    }()
+//    
+//    private let nameBabyLabel: UILabel = {
+//        let nameAvatar = UILabel()
+//        nameAvatar.text = "Дети"
+//        nameAvatar.frame = CGRect(x: 80, y: 220, width: 50, height: 20)
+//        nameAvatar.font = .boldSystemFont(ofSize: 18)
+//        nameAvatar.textColor = .white
+//        return nameAvatar
+//    }()
+//    
+//    private let nameAgeLabel: UILabel = {
+//        let nameAvatar = UILabel()
+//        nameAvatar.text = "12+"
+//        nameAvatar.frame = CGRect(x: 80, y: 240, width: 50, height: 20)
+//        nameAvatar.font = .boldSystemFont(ofSize: 14)
+//        nameAvatar.textColor = .systemGray
+//        return nameAvatar
+//    }()
     
     private let oneButton: UIButton = {
         var configuration = UIButton.Configuration.filled()
@@ -115,28 +125,24 @@ class ProfileUIView: UIView {
         return button
     }()
     
-    private let imageSettingAvatar: UIImageView = {
-        let imageView = UIImageView()
-        imageView.image = UIImage(named: "setting")
-        imageView.contentMode = .scaleAspectFit
-        imageView.frame = CGRect(x: 10, y: 270, width: 60, height: 60)
-        imageView.layer.cornerRadius = 30
-        let borderColor = UIColor(red: 2/255, green: 2/255, blue: 2/255, alpha: 1)
-        imageView.layer.borderColor = borderColor.cgColor
-        imageView.layer.borderWidth = 0
-        imageView.layer.masksToBounds = true
-        return imageView
-    }()
-    
-    private lazy var settingButton: UIButton = {
-        let button = UIButton()
-        button.setTitle("Редактировать", for: .normal)
-        button.setTitleColor(.white, for: .normal)
-        button.clipsToBounds = true
-        button.frame = CGRect(x: 65, y: 270, width: 150, height: 60)
+    private let settingButton: UIButton = {
+        var configuration = UIButton.Configuration.filled()
+        configuration.image = UIImage(named: "setting")
+        // между контентом
+        configuration.imagePadding = 10
+        configuration.imagePlacement = .leading
+        configuration.titleAlignment = .leading
+        configuration.baseBackgroundColor = .systemGray6
+        configuration.attributedTitle = AttributedString("Редактировать", attributes: AttributeContainer([NSAttributedString.Key.foregroundColor: UIColor(white: 230/120, alpha: 1)]))
+        configuration.buttonSize = .large
+        configuration.contentInsets = .init(top: 5, leading: -5, bottom: 5, trailing: 55)
+        configuration.cornerStyle = .large
+        let button = UIButton(configuration: configuration, primaryAction: UIAction(handler: { action in
+          //debugPrint("Tapped One Button!")
+        }))
+        button.frame = CGRect(x: 10, y: 270, width: 250, height: 60)
         return button
     }()
-    
     
     
     override init(frame: CGRect) {
@@ -146,13 +152,14 @@ class ProfileUIView: UIView {
         containerView.addSubview(myImageAvat)
         containerView.addSubview(nameAvatarLabel)
         containerView.addSubview(nameUrlAvatarLabel)
-        containerView.addSubview(imageBabyAvatar)
-        containerView.addSubview(nameBabyLabel)
-        containerView.addSubview(nameAgeLabel)
+        
+//        containerView.addSubview(imageBabyAvatar)
+//        containerView.addSubview(nameBabyLabel)
+//        containerView.addSubview(nameAgeLabel)
         containerView.addSubview(oneButton)
-        containerView.addSubview(imageSettingAvatar)
         containerView.addSubview(settingButton)
         addSubview(containerView2)
+        containerView.addSubview(babyButton)
         backgroundColor = .systemBackground
     }
     
@@ -166,3 +173,31 @@ class ProfileUIView: UIView {
 
     
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//    var gradientLayer: CAGradientLayer! {
+//        didSet {
+//            gradientLayer.startPoint = CGPoint(x: 0, y: 0)
+//            gradientLayer.endPoint = CGPoint(x: 0, y: 1)
+//            let startColor = #colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1).cgColor
+//            let endColor = #colorLiteral(red: 0.1411764771, green: 0.3960784376, blue: 0.5647059083, alpha: 1).cgColor
+//            gradientLayer.colors = [startColor, endColor]
+//            //   gradientLayer.locations = [0, 0.2, 1]
+//        }
+//    }
